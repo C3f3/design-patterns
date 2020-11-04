2f4f942f05b935cab6181442d1f92dfba8e51b4f
sesantanav <sesantanav@gmail.com>
2020-11-04 14:48:19 -0300

    Descripciones metodos

--- ejemplos/metodos.py
+++ ejemplos/metodos.py
@@ -1,22 +1,31 @@
 """
 Metodos en python
+Haciendo uso de diferentes decorators (decoradores) es posible crear diferentes
+tipos de  de métodos.
 
 1.- Métodos Estáticos
-@staticmethod
+decorator -> @staticmethod
 No necesitan una instancia de la clase para ser utilizados
 No tienen acceso al exterior, no pueden acceder a otro atributo o
 llamar a otra función de la clase
 
 
 2.- Métodos de clase
+decorator -> @classmethod
+Reciben el argumento cls, que hace referencia a la clase, por lo tanto, es
+posible acceder a la clase y no a la instancia.
+No pueden acceder a los atributos de la instancia
+Si pueden modificar los atributos de la clase
 
 3.- Métodos de instancia
+Son los métodos normales, reciben como parámetro de entrada self que hace
+referencia a la instancia que llama al método. También reciben otros argumentos
+como entrada.
+
 """
 
-# Ejemplo Método Estático
+# Ejemplo
 class Imprimir:
-
-    
     # Metodo de instancia
     def info_param(self, param1, param2):
         print("Ejemplo Suma:")
